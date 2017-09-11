dac558958e8d9cd819353877a992f1a6cc9ac526
Chess Seventh <chess.seventh@gmail.com>
2017-09-11 20:29:43 +0200

    wav formatter fixes

--- src/wav-decoder/wav_formatter.py
+++ src/wav-decoder/wav_formatter.py
@@ -14,7 +14,7 @@ import time
 BASE_RATE = 20800
 NOAA_LINE_LENGTH = 2080
 
-class WavFormatter(object):
+class WAVFormatter(object):
     """
     Object to handle the a WAV file.
     Runs a series of tests to make sure the file is proper for handling.
@@ -24,7 +24,7 @@ class WavFormatter(object):
         Init and run tests.
         """
         self.filename = wav_fn
-        self.workfile = 'resampled.wav'
+        self.workfile = 're.wav'
 
         self.rate = None
 
@@ -33,61 +33,62 @@ class WavFormatter(object):
 
         self.health_check()
 
-    def get_wav(self):
-        """
-        Get the wav.
-        """
-        return scipy.io.wavfile.read(self.workfile)
-
     def health_check(self):
         """
         Check health of input file.
         """
         with wave.open(self.filename, 'rb') as f:
             if f.getframerate() is not BASE_RATE:
-                if not self.resample_rate(f):
+                if not self.resample_rate():
                     print('something failed in resample')
                 if not self.write_resampled():
                     print('something failed in writing resampled file.')
 
             if f.getnchannels() is not 1:
-                if not to_mono(f):
+                if not to_mono():
                     print('something failed.')
 
     def write_resampled(self):
         """
         Write the workfile with the data.
         """
-        try:
-            scipy.io.wavfile.write(self.workfile, self.rate, self.signal)
-            return True
-        except:
-            return False
+        # try:
+        scipy.io.wavfile.write(self.workfile, self.rate, self.signal)
+        return True
+        # except:
+        #     return False
 
-    def resample_rate(self, f):
+    def resample_rate(self):
         """
         Resample to correct rate
         """
-        try:
-            if not f:
-                (self.rate, self.signal) = scipy.io.wavfile.read(f)
-                coef = BASE_RATE / float(self.rate)
-                self.samples = int(coef * len(self.signal))
-                self.signal = scipy.signal.resample(self.signal, self.samples)
-                return True
-        except:
-            return False
+        # try:
+        # with wave.open(self.filename, 'rb') as f:
+        (self.rate, self.signal) = scipy.io.wavfile.read(self.filename)
+        coef = BASE_RATE / float(self.rate)
+        self.samples = int(coef * len(self.signal))
+        self.signal = scipy.signal.resample(self.signal, self.samples)
+        return True
+        # except:
+        #     return False
 
-    def to_mono(self, f):
+    def to_mono(self):
         """
         Stereo to Mono converter.
         """
-        try:
+        # try:
+        with wave.open(self.filename, 'rb') as f:
             with wave.open(self.workfile, 'wb') as mono:
                 mono.setparams(f.getparams())
                 mono.setnchannels(1)
                 mono.writeframes(audioop.tomono(f.readframes(float('inf')), f.getsampwidth(), 1, 1))
-            return True
+        return True
 
-        except:
-            return False
+        # except:
+        #     return False
+
+    def get_wav(self):
+        """
+        Get the wav.
+        """
+        return scipy.io.wavfile.read(self.workfile)
